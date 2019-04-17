390a201eac4b1b71b0e0a2c25ebb726c249d0b09
Julie K <juliekdang@gmail.com>
2019-04-17 02:04:31 -0400

    Sample async repository approach WIP
    (not in use)

---
+++ foolapi/Repository/IProductsRepository.cs
@@ -0,0 +1,18 @@
+﻿using foolapi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// currently unused - for later use
+namespace foolapi.Repository
+{
+    public interface IProductsRepository
+    {
+        Task Add(Product item);
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> Find(int id);
+        Task Remove(int id);
+        Task Update(Product item);
+
+        bool CheckValidUserKey(string reqkey);
+    }
+}
